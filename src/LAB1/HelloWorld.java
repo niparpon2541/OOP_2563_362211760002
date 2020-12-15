b40465c02c88a2595869500fef5b965fcdad5908
niparpon2541 <75650627+niparpon2541@users.noreply.github.com>
2020-12-15 08:48:52 +0700

    Update HelloWorld.java

--- src/LAB1/HelloWorld.java
+++ src/LAB1/HelloWorld.java
@@ -5,6 +5,9 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         System.out.print("Hello World");
+        System.out.print("Hello World");
+        System.out.print("Hello World");
+        System.out.print("Hello World");
 
     }//main
 
